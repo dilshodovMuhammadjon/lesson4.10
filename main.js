50f8b8bf0810e8d57b0b64a7db61b07fee18f0c6
unknown <voldam@ lolamgit config --global user.email volidam>
2024-03-27 09:28:37 +0300

    it was my homework

---
+++ main.js
@@ -0,0 +1,14 @@
+// let hisob = () => {
+// console.log()
+// }
+function max(a,b){
+    let c;
+    c = Math.max(a,b);
+    alert("kritilgan 2ta sonning katasi =" + c);
+}
+
+
+let brinchi = Number(prompt ("1 chi soni kriting..."));
+let ikkinchi = Number(prompt ("2 chi soni kriting..."));
+
+max(brinchi,ikkinchi);
